85f34efe156eada7973a90952ef82ac18d3b0137
Jannes Klee <jannes.klee@proton.me>
2022-12-11 14:00:40 +0100

    ENH / change structure for day5 for second part

--- day5/supply_stacks.py
+++ day5/supply_stacks.py
@@ -2,17 +2,16 @@
 # -*- coding: utf-8 -*-
 
 import re
+from abc import ABC, abstractmethod
 
-with open('input.txt') as f:
-    lines = f.readlines()
-
-    # read header to find out number of stacks
+def read_header(lines: list) -> (list, int):
     for line_number, line in enumerate(lines):
         if line.startswith(' 1 '):
             end_of_header = line_number
             stack_numbers = [int(entry) for entry in re.findall(r' \d+ ', line)]
+    return (stack_numbers, end_of_header)
 
-    # create stacks
+def create_stacks(lines: list, stack_numbers: list, end_of_header: int) -> list:
     stack = [[] for stack_entry in stack_numbers]
     for line in lines[end_of_header-1::-1]:
         splitted_line = [line[i:i+4] for i in range(0, len(line), 4)]
@@ -21,16 +20,54 @@ with open('input.txt') as f:
             crate = crate[crate.find("[")+1:crate.find("]")]
             if crate != '':
                 stack[stack_number-1].append(crate)
-    
-    # movements on stacks
+    return stack
+
+# distinguish between different movers with classes
+class CrateMover(ABC):
+    @abstractmethod
+    def move_crates(self, stack: list, movement: dict):
+        pass
+
+class CrateMover9000(CrateMover):
+    def move_crates(self, stack: list, movement: dict):
+        for _ in range(movement['amount']):
+             item_to_move = stack[movement['origin']-1].pop()
+             stack[movement['destination']-1].append(item_to_move)
+        return stack
+
+class CrateMover9001(CrateMover):
+    def move_crates(self, stack: list, movement: dict):
+        pass
+
+
+def crate_movements(lines: list, end_of_header: int, 
+                    stack: list, mover: CrateMover) -> list:
+
     for line in lines[end_of_header+2:]:
         moving  = re.findall(r' \d+', line)
         amount = int(moving[0])
         origin = int(moving[1])
         destination = int(moving[2])
-        for _ in range(amount):
-            item_to_move = stack[origin-1].pop()
-            stack[destination-1].append(item_to_move)
+
+        movement = {'amount': amount,
+                    'origin': origin,
+                    'destination': destination}
+
+        crates = mover.move_crates(stack, movement)
+
+    return stack
+ 
+
+# part 1
+with open('input.txt') as f:
+    lines = f.readlines()
+
+    stack_numbers, end_of_header = read_header(lines)
+
+    stack = create_stacks(lines, stack_numbers, end_of_header)
+   
+    MuddyMover = CrateMover9000()
+    stack = crate_movements(lines, end_of_header, stack, MuddyMover)
 
     # get last items
     result_list = []
@@ -39,5 +76,5 @@ with open('input.txt') as f:
 
     print(result_list)
 
-        
+
 
